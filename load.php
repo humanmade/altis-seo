bb2d13a6e1b0e47ba062ab09e12e07a019decf73
Robert O'Rourke <robert@o-rourke.org>
2019-10-01 14:59:28 +0100

    Support new autoload method

--- load.php
+++ load.php
@@ -9,14 +9,6 @@ namespace Altis\SEO; // @codingStandardsIgnoreLine
 
 use function Altis\register_module;
 
-require_once __DIR__ . '/inc/namespace.php';
-require_once __DIR__ . '/inc/site_verification/namespace.php';
-
-// Do not initialise if plugin.php hasn't been included yet.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
 add_action( 'altis.modules.init', function () {
 	$default_settings = [
 		'enabled' => true,

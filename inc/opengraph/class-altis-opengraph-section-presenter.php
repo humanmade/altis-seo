c44ddb19c46d0f994a757790edb56de111eaeaf6
Chris Reynolds <chris@jazzsequence.com>
2021-06-28 15:05:25 -0600

    add new presenters
    including a working version of the opengraph author presenter

---
+++ inc/opengraph/class-altis-opengraph-section-presenter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Opengraph Meta Tag additions
+ *
+ * @package altis/seo
+ */
+
+namespace Altis\SEO;
+
+use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
+
+class Altis_Opengraph_Section_Presenter extends Abstract_Indexable_Tag_Presenter {
+	public function present() {
+
+	}
+
+	public function get() {
+
+	}
+}

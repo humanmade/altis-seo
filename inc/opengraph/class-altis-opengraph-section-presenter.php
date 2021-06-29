32827dd3500fdbaa833f8ffb7a6e9217b897cec8
Chris Reynolds <chris@jazzsequence.com>
2021-06-29 15:16:38 -0600

    flesh out the section provider

--- inc/opengraph/class-altis-opengraph-section-presenter.php
+++ inc/opengraph/class-altis-opengraph-section-presenter.php
@@ -9,12 +9,33 @@ namespace Altis\SEO;
 
 use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
 
+/**
+ * Adds a custom Opengraph Article Author meta tag.
+ */
 class Altis_Opengraph_Section_Presenter extends Abstract_Indexable_Tag_Presenter {
-	public function present() {
+	/**
+	 * The tag format including placeholders.
+	 *
+	 * @var string
+	 */
+	protected $tag_format = '<meta property="og:article:section" content="%s" />';
 
-	}
-
+	/**
+	 * Returns a category name for the new section meta tag.
+	 *
+	 * @return void|string The category if we're on a singular post.
+	 */
 	public function get() {
+		if ( ! is_single() ) {
+			return;
+		}
 
+		$terms = get_the_terms( $this->presentation->model->object_id, 'category' );
+
+		if ( ! $terms ) {
+			return;
+		}
+
+		return $terms[0]->name;
 	}
 }

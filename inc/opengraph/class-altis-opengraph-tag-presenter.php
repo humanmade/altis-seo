b39cf3ed50f5a745f39af8eb3fc070e89940fd5e
Chris Reynolds <chris@jazzsequence.com>
2021-06-29 15:16:16 -0600

    add a tag presenter class
    that's another thing that the meta tags thing did that yoast doesn't do

---
+++ inc/opengraph/class-altis-opengraph-tag-presenter.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Opengraph Meta Tag additions
+ *
+ * @package altis/seo
+ */
+
+namespace Altis\SEO;
+
+use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
+
+/**
+ * Adds a custom Opengraph Article Tag meta tag.
+ */
+class Altis_Opengraph_Tag_Presenter extends Abstract_Indexable_Tag_Presenter {
+	/**
+	 * The tag format including placeholders.
+	 *
+	 * @var string
+	 */
+
+	public function present() {
+		$terms = explode( ',', $this->get() );
+		$tag_format = '<meta property="og:article:tag" content="%s" />';
+		$output = '';
+
+		foreach ( $terms as $term ) {
+			$output .= sprintf( $tag_format, $term ) . "\n";
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Returns a Tag name for the new section meta tag.
+	 *
+	 * @return void|string The first tag if we're on a singular post.
+	 */
+	public function get() {
+		if ( ! is_single() ) {
+			return;
+		}
+
+		$terms = get_the_terms( $this->presentation->model->object_id, 'post_tag' );
+
+		if ( ! $terms ) {
+			return;
+		}
+
+		$term_names = wp_list_pluck( $terms, 'name' );
+		return implode( ',', $term_names );
+	}
+}

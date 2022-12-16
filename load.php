219392834a81290061ef4d5bb08ea8f485318b61
Mike Little <mike@zed1.com>
2022-12-16 17:39:51 +0000

    Remove Yoast SEO related functionality.

--- load.php
+++ load.php
@@ -13,28 +13,6 @@ add_action( 'altis.modules.init', function () {
 	$default_settings = [
 		'enabled' => true,
 		'redirects' => true,
-		'metadata' => [
-			'opengraph' => true,
-			'twitter' => true,
-			'fallback-image' => false,
-			'pinterest-verify' => false,
-			'social-urls' => [
-				'facebook' => '',
-				'twitter' => '',
-				'instagram' => '',
-				'linkedin' => '',
-				'myspace' => '',
-				'pinterest' => '',
-				'youtube' => '',
-				'wikipedia' => '',
-			],
-			'opengraph-fallback' => [
-				'frontpage-title' => '',
-				'frontpage-desc' => '',
-				'frontpage-image' => '',
-			],
-		],
-		'site-verification' => true,
 	];
 	$options = [
 		'defaults' => $default_settings,

b9b18970689a23a57ae88ba575dc45a058883741
Mike Little <mike@zed1.com>
2022-12-16 16:13:36 +0000

    Remove robots.txt functionality from Altis

--- tests/functional/MetaCest.php
+++ tests/functional/MetaCest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tests for SEO module's meta tags, robots, and sitemap features.
+ * Tests for SEO module's meta tags and sitemap features.
  * phpcs:disable WordPress.Files, WordPress.NamingConventions, PSR1.Classes.ClassDeclaration.MissingNamespace, HM.Functions.NamespacedFunctions
  */
 
@@ -43,26 +43,6 @@ class MetaCest {
 		] );
 
 		$I->seeElement( 'script.yoast-schema-graph' );
-	}
-
-	/**
-	 * Test robots.txt output.
-	 *
-	 * @param FunctionalTester $I Functional tester object.
-	 *
-	 * @return void
-	 */
-	public function test_robotstxt( FunctionalTester $I ) {
-		$I->amOnPage( '/robots.txt' );
-		$content = $I->grabPageSource();
-
-		$text = <<<EOL
-User-agent: *
-Disallow: /wp-admin/
-Allow: /wp-admin/admin-ajax.php
-EOL;
-
-		$I->assertEquals( trim( $text ), trim( $content ) );
 	}
 
 	/**
